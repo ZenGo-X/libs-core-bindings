8755aa551ac7b7905dfd0c22427f7e22b328b39c
gbenattar <g.benattar@gmail.com>
2018-10-10 12:04:06 +0300

    Remove lib types

---
+++ target/debug/deps/libs_core_bindings-5b087ac42e018aed.d
@@ -0,0 +1,9 @@
+/Users/gary/Coding/libs-core-bindings/target/debug/deps/liblibs_core_bindings-5b087ac42e018aed.rlib: src/lib.rs src/protocols/mod.rs src/protocols/two_party_ecdsa/mod.rs src/utilities/mod.rs src/utilities/json_utils.rs
+
+/Users/gary/Coding/libs-core-bindings/target/debug/deps/libs_core_bindings-5b087ac42e018aed.d: src/lib.rs src/protocols/mod.rs src/protocols/two_party_ecdsa/mod.rs src/utilities/mod.rs src/utilities/json_utils.rs
+
+src/lib.rs:
+src/protocols/mod.rs:
+src/protocols/two_party_ecdsa/mod.rs:
+src/utilities/mod.rs:
+src/utilities/json_utils.rs:
